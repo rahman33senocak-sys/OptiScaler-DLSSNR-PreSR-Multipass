3497d9fa727641fbb145bb10e8d8a216e1b583b7
rahman33senocak-sys <rahman33senocak@gmail.com>
2026-09-21 23:31:54 +0300

    Prepare direct NR composition from game output

--- OptiScaler/shaders/dlssnr/DlssNr_Dx12_State.h
+++ OptiScaler/shaders/dlssnr/DlssNr_Dx12_State.h
@@ -274,6 +274,7 @@ struct DlssNr_Dx12::State
             unsigned nextMarker = 0, lastMarker = 0;
             bool everRecorded = false, smallReadable = false, reset = true, failed = false;
             bool rayReconstruction = false, finishedPicture = false, privateRr = false;
+            bool directOutputRead = false;
             DlssNr::PrivateUpscaler backend = DlssNr::PrivateUpscaler::DLSS;
             std::unique_ptr<DlssNr::PrivateUpscalerDx12> upscaler;
             DlssNr::PrivateUpscalerFrameDx12 frame;
